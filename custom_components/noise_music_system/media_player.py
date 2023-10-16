5a7c194932947b4673fb2524282ab9c940b02de1
Bill Swanson <bswanson58@gmail.com>
2023-10-16 15:16:03 -0500

    Code cleanup, defined literals for most strings.

--- custom_components/noise_music_system/media_player.py
+++ custom_components/noise_music_system/media_player.py
@@ -8,17 +8,15 @@ import logging
 import json
 from typing import Any
 
-from homeassistant.components.media_player.const import (
-    MediaPlayerEntityFeature,
-    MEDIA_TYPE_MUSIC,
-    SUPPORT_NEXT_TRACK,
-    SUPPORT_PAUSE,
-    SUPPORT_PLAY,
-    SUPPORT_PREVIOUS_TRACK,
-    SUPPORT_VOLUME_SET,
-    SUPPORT_VOLUME_STEP,)
-
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.components import mqtt
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 import homeassistant.helpers.config_validation as cv
+from homeassistant.util import dt as dt_util, slugify
+from homeassistant.util.json import json_loads
+
+from homeassistant.components.media_player.const import MediaPlayerEntityFeature
 from homeassistant.components.media_player import (
     PLATFORM_SCHEMA,
     MediaPlayerEntity,
@@ -34,12 +32,6 @@ from homeassistant.const import (
     CONF_DEVICE_ID,
     CONF_NAME
 )
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.components import mqtt
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-from homeassistant.util import dt as dt_util, slugify
-from homeassistant.util.json import json_loads
 
 _LOGGER = logging.getLogger("noisemusicsystem")
 
@@ -49,17 +41,24 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 SUPPORT_FEATURES = (
-    MediaPlayerEntityFeature.NEXT_TRACK
+      MediaPlayerEntityFeature.NEXT_TRACK
     | MediaPlayerEntityFeature.PAUSE
     | MediaPlayerEntityFeature.PLAY
+    | MediaPlayerEntityFeature.PLAY_MEDIA
     | MediaPlayerEntityFeature.PREVIOUS_TRACK
     | MediaPlayerEntityFeature.SEEK
     | MediaPlayerEntityFeature.STOP
+    | MediaPlayerEntityFeature.REPEAT_SET
     | MediaPlayerEntityFeature.VOLUME_MUTE
     | MediaPlayerEntityFeature.VOLUME_SET
+    | MediaPlayerEntityFeature.VOLUME_STEP
 )
 
-SUBSCRIBE_TOPIC = "noisemusicsystem/#"
+# MQTT strings for topics/properties
+TOPIC_BASE = 'noisemusicsystem'
+SUBSCRIBE_TOPIC = f'{TOPIC_BASE}/#'
+
+ONLINE_STATUS = 'online'
 
 ATTR_ARTIST = 'artist'
 ATTR_ALBUM = 'album'
@@ -74,6 +73,20 @@ ATTR_PLAY_STATE = 'playstate'
 
 PLAY_STATE_PLAYING = 'playing'
 
+# Topic strings
+COMMAND_TOPIC = 'command'
+
+COMMAND_NEXT_TRACK = 'next'
+COMMAND_MUTE = 'mute'
+COMMAND_PAUSE = 'pause'
+COMMAND_PLAY = 'play'
+COMMAND_REPEAT = 'repeat'
+COMMAND_SEEK = 'seek'
+COMMAND_STOP = 'stop'
+COMMAND_PREVIOUS_TRACK = 'previous'
+COMMAND_VOLUME = 'volume'
+
+# The expected MQTT status structure
 STATUS_PAYLOAD = vol.Schema(
     vol.All(
         json_loads,
@@ -95,15 +108,11 @@ STATUS_PAYLOAD = vol.Schema(
     )
 )
 
+# The MQTT playload for transmitted commands
 class command_payload:
     def __init__(self, command: str, parameter: str = '') -> None:
         self.command: str = command
         self.parameter: str = parameter
-
-
-def _slugify_upper(string: str) -> str:
-    """Return a slugified version of string, uppercased."""
-    return slugify(string).upper()
 
 
 async def async_setup_platform(
@@ -135,6 +144,7 @@ class NoiseMusicSystem(MediaPlayerEntity):
         self._name = device[CONF_NAME]
         self._device_name = device[CONF_DEVICE_ID]
         self._device_id = _slugify_upper(device[CONF_DEVICE_ID])
+# TODO: define the unique_id
         self._attr_unique_id = "unique_id"
 
         self._attr_media_album_artist = ""
@@ -144,24 +154,25 @@ class NoiseMusicSystem(MediaPlayerEntity):
         self._album_art = None
         self._volume = 0.0
         self._attr_state = MediaPlayerState.PAUSED
-        self._attr_available = True
+        self._attr_available = False
 
     async def async_added_to_hass(self) -> None:
         """Subscribe to MQTT events."""
 
         def update_availability(payload: str) -> None:
             """Update device availability"""
-            self._attr_available = payload == "online"
+            self._attr_available = payload == ONLINE_STATUS
             self.schedule_update_ha_state(True)
 
-        def update_state(payload: str) -> None:
+        def update_status(payload: str) -> None:
             """Update device state"""
             try:
                 data = STATUS_PAYLOAD(payload)
             except vol.MultipleInvalid as error:
-                _LOGGER.debug("Skipping update because of malformatted data: %s", error)
+                _LOGGER.debug(f'Status update has malformatted data: {error}')
                 return
 
+            self._attr_available = True
             self._track_artist = data.get(ATTR_ARTIST)
             self._attr_media_album_artist = data.get(ATTR_ARTIST)
             self._track_album_name = data.get(ATTR_ALBUM)
@@ -171,7 +182,6 @@ class NoiseMusicSystem(MediaPlayerEntity):
             self._attr_media_position = data.get(ATTR_POSITION)
             self._volume = data.get(ATTR_VOLUME) / 100.0
             self._attr_is_volume_muted = data.get(ATTR_VOLUME_MUTED)
-            self._attr_available = True
             self._attr_state = MediaPlayerState.PLAYING if data.get(ATTR_PLAY_STATE) == PLAY_STATE_PLAYING else MediaPlayerState.PAUSED
             if(len(data.get(ATTR_POSITION_AT))):
                 self._attr_media_position_updated_at = dt.datetime.strptime(data.get(ATTR_POSITION_AT), '%a, %d %b %Y %H:%M:%S GMT') #RFC 1123 format
@@ -183,21 +193,20 @@ class NoiseMusicSystem(MediaPlayerEntity):
             """Handle new MQTT messages."""
             try:
                 topic_info = _parse_topic(msg.topic)
-                if topic_info.get(CONF_DEVICE_ID) == self._device_id:
-                    data = msg.payload
-                    command = topic_info.get(ATTR_COMMAND)
 
-                    match command:
+                if topic_info.get(CONF_DEVICE_ID) == self._device_id:
+                    match topic_info.get(ATTR_COMMAND):
                         case 'availability':
-                            update_availability(data)
+                            update_availability(msg.payload)
                         case 'status':
-                            update_state(data)
+                            update_status(msg.payload)
 
             except vol.MultipleInvalid as error:
-                _LOGGER.debug("Skipping update because of malformatted data: %s", error)
+                _LOGGER.debug(f'Received message has malformatted data: {error}')
                 return
 
         await mqtt.async_subscribe(self.hass, SUBSCRIBE_TOPIC, message_received, 1)
+
 
     # Exposed properties
     @property
@@ -272,50 +281,55 @@ class NoiseMusicSystem(MediaPlayerEntity):
     async def publish_command(self, command: str, parameter: str = '') -> None:
         """Publish a command"""
         payload = command_payload(command, parameter)
-        command_topic = "noisemusicsystem/% s/command" % (self._device_name)
+        command_topic = f"{TOPIC_BASE}/{self._device_name}/{COMMAND_TOPIC}"
 
         await mqtt.async_publish(self.hass, command_topic, json.dumps(vars(payload)))
 
     async def async_media_play(self) -> None:
         """Send play command."""
-        await self.publish_command('play')
+        await self.publish_command(COMMAND_PLAY)
 
     async def async_media_pause(self) -> None:
         """Send pause command."""
-        await self.publish_command('pause')
+        await self.publish_command(COMMAND_PAUSE)
 
     async def async_media_stop(self) -> None:
         """Send stop command."""
-        await self.publish_command('stop')
+        await self.publish_command(COMMAND_STOP)
 
     async def async_media_previous_track(self) -> None:
         """Send previous track command."""
-        await self.publish_command('previous')
+        await self.publish_command(COMMAND_PREVIOUS_TRACK)
 
     async def async_media_next_track(self) -> None:
         """Send next track command."""
-        await self.publish_command('next')
+        await self.publish_command(COMMAND_NEXT_TRACK)
 
     async def async_media_seek(self, position: float) -> None:
         """Send seek command."""
-        await self.publish_command('seek', position)
+        await self.publish_command(COMMAND_SEEK, position)
 
     async def async_set_repeat(self, repeat: RepeatMode) -> None:
         """Set repeat mode."""
-        await self.publish_command('repeat')
+        await self.publish_command(COMMAND_REPEAT)
 
     async def async_set_volume_level(self, volume: float) -> None:
         """Set volume level, range 0..1."""
-        await self.publish_command('volume', volume)
+        await self.publish_command(COMMAND_VOLUME, volume)
 
     async def async_mute_volume(self, mute: bool) -> None:
         """Mute the volume."""
-        await self.publish_command('mute', mute)
+        await self.publish_command(COMMAND_MUTE, mute)
+
+
+def _slugify_upper(string: str) -> str:
+    """Return a slugified version of string, uppercased."""
+    return slugify(string).upper()
 
 
 def _parse_topic(topic: str) -> dict[str, Any]:
     """Parse the mqtt topic string"""
-    parts = topic.split("/")
+    parts = topic.split('/')
     if len(parts) < 3:
         return
 
